9438915909e7baf7ac3da312ccbb164fd7456c8f
leonar2if <Leonar20081007@gmail.com>
2026-08-15 12:05:53 -0400

    Update PasswordField.kt

--- app/src/main/java/com/example/ui/components/PasswordField.kt
+++ app/src/main/java/com/example/ui/components/PasswordField.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
